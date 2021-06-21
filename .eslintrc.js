1252b3b05badac8c7be5ac553c9ecdd2746076ac
Mitanshu Patel <mitanshu.u.patel@gmail.com>
2021-06-21 10:27:22 +0530

    setup eslint & prettier

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  parser: "@babel/eslint-parser",
+  extends: [
+    "plugin:react/recommended",
+    "airbnb",
+    "plugin:prettier/recommended",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react", "prettier"],
+  rules: {
+    "react/jsx-filename-extension": [
+      2,
+      {
+        extensions: [".js", ".jsx"],
+      },
+    ],
+    "prettier/prettier": "error",
+    "arrow-body-style": "off",
+    "prefer-arrow-callback": "off",
+  },
+};
--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello</h1>, document.getElementById('root'))
+// Rules
+// 1. Name should should start with capital letter
+// 2. return only single element
+// 3. use className instead of class
+
+const App = () => (
+  <>
+    <h1 className="header">Hello World</h1>
+    <h2>yagnesh</h2>
+  </>
+);
+
+ReactDOM.render(<App />, document.getElementById("root"));
